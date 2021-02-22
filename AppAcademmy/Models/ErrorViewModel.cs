183f46234be870cf66b4342bf49309e01c32795b
MARCELOPICCOLO <marcelociabatari@hotmail.com>
2021-02-22 19:58:40 -0300

    projeto inicial

---
+++ AppAcademmy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppAcademmy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
